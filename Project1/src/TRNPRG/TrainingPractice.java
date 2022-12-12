3c2025b6d2b9a1f35d73a2c9def0a389ad1d8001
Architect​ <xyz@gmail.com>
2022-12-12 14:52:15 +0530

    commit from Architect

--- Project1/src/TRNPRG/TrainingPractice.java
+++ Project1/src/TRNPRG/TrainingPractice.java
@@ -5,6 +5,7 @@ public class TrainingPractice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is content of Master Branch");
+System.out.println("This is content of Architect1 Branch");
 	}
 
 }
